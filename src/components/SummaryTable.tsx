a1753715124b6b150d903f23e9a519518fdc4708
Pedro <pedrovs3@hotmail.com>
2023-01-18 16:19:05 -0300

    finalizando visual da web

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -1,4 +1,5 @@
 import {HabitDay} from "./HabitDay";
+import {generateRangeBetweenDates} from "../utils/generate-range-between-dates";
 
 const weekDays = ['D',
 	'S',
@@ -9,9 +10,13 @@ const weekDays = ['D',
 	'S'
 ]
 
+const summaryDates = generateRangeBetweenDates();
+const MINIMUM_SUMMARY_DATES_SIZE = 18 * 7 // 18 weeks
+const amountOfDaysToFill = MINIMUM_SUMMARY_DATES_SIZE - summaryDates.length
+
 export const SummaryTable = () => {
   return (
-		<div className={'w-full flex'}>
+		<div className={'w-full flex justify-center my-10'}>
 			<div className={'grid grid-rows-7 grid-flow-row gap-3'}>
 				{weekDays.map((weekDay, index) => {
 					return (
@@ -23,17 +28,17 @@ export const SummaryTable = () => {
 			</div>
 
 			<div className={'grid grid-rows-7 grid-flow-col gap-3'}>
-				<HabitDay/>
-				<HabitDay/>
-				<HabitDay/>
-				<HabitDay/>
-				<HabitDay/>
-				<HabitDay/>
-				<HabitDay/>
-				<HabitDay/>
-				<HabitDay/>
-				<HabitDay/>
+				{summaryDates.map(date => {
+					return (
+							<HabitDay key={date.toString()}/>
+					)
+				})}
 
+				{amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill }).map((_, index) => {
+					return (
+							<div key={index} className={'w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed'}/>
+					)
+				})}
 			</div>
 		</div>
 	)
--- src/utils/generate-range-between-dates.ts
+++ src/utils/generate-range-between-dates.ts
@@ -1,1 +1,16 @@
-export function generateRangeBetweenDates
+import dayjs from "dayjs";
+
+export const generateRangeBetweenDates = () => {
+	const firstDayOfTheYear =dayjs().startOf('year');
+	const today = new Date();
+
+	const dates = [];
+	let compareDate = firstDayOfTheYear
+
+	while (compareDate.isBefore(today)) {
+		dates.push(compareDate.toDate());
+		compareDate = compareDate.add(1, 'day');
+	};
+
+	return dates;
+};
